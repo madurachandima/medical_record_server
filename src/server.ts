1f7b870684fbbb1c359ccd6af43af0e616f8332e
Madura Dias <madurachandima6@gmail.com>
2026-04-15 19:20:44 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,20 @@ const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI || ""
 
 
+// mongoose.connect(MONGO_URI).then(() => {
+//     console.log("Connected to MongoDB");
+//     app.listen(PORT, () => {
+//         console.log(`Server is running on port ${PORT}`);
+//     })
+// }).catch((error) => {
+//     console.error('MongoDB connection error:', error);
+//     process.exit(1);
+// })
+
+
 mongoose.connect(MONGO_URI).then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
+    app.listen({ port: PORT, host: "0.0.0.0" }, () => {
         console.log(`Server is running on port ${PORT}`);
     })
 }).catch((error) => {
